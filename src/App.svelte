f97f37806bb09a7a101cec5b2b367995f48e894d
Yang Chen <yyangc88@gmail.com>
2023-01-08 15:25:45 -0500

    Display client auth errors; login mobile layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import Chatboard from "./lib/Chatboard.svelte";
   import Login from "./lib/Login.svelte";
+  import { currentUser } from "./lib/pocketbase";
 </script>
 
-<nav class="p-2 bg-gray-800/90 border-b-2 sticky top-0 z-50 mb-4">
-  <p class="text-2xl font-bold text-white">Yin Messaging</p>
-</nav>
-<div class="h-[90vh] mx-10 bg-gray-700 rounded-lg">
-  <Login />
+<div class="h-[90vh] mx-10 rounded-lg">
+  {#if $currentUser}
+    <div class="h-full relative">
+      <Chatboard />
+    </div>
+  {:else}
+    <Login />
+  {/if}
 </div>
--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -3,71 +3,174 @@
     import { currentUser, pb } from "./pocketbase";
     import Messages from "./Messages.svelte";
     import Chatboard from "./Chatboard.svelte";
+    import Icon from "@iconify/svelte";
 
-    let username: string;
-    let password: string;
+    let username = "";
+    let password = "";
+    let passwordConfirm = "";
+    let firstName = "";
+    let lastName = "";
+    let clientError: ClientResponseError;
+    let isRegistering = false;
 
     async function login() {
-        await pb.collection("users").authWithPassword(username, password);
+        try {
+            await pb.collection("users").authWithPassword(username, password);
+        } catch (err) {
+            clientError = err as ClientResponseError;
+        }
     }
 
     async function signUp() {
         try {
             const data = {
                 username,
-                name: username,
+                firstName,
+                lastName,
                 password,
-                passwordConfirm: password,
+                passwordConfirm: passwordConfirm,
             };
             const createdUser = await pb.collection("users").create(data);
             await login();
         } catch (err) {
-            // TODO: Handle error
-            const clientErr = err as ClientResponseError;
-            console.log({ clientErr });
+            clientError = err as ClientResponseError;
         }
     }
 </script>
 
-{#if $currentUser}
-    <div class="h-full relative">
-        <Chatboard />
-        <!-- <button on:click={signOut}>Sign Out</button> -->
-    </div>
-    <!-- <Messages /> -->
-{:else}
-    <form
-        action="#"
-        on:submit|preventDefault
-        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-4"
-    >
-        <div>
-            <label
-                for="username"
-                class="block text-gray-700 text-sm font-bold mb-2"
-                >Username</label
-            >
-            <input
-                type="text"
-                name="username"
-                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                bind:value={username}
+<h1 class="fixed text-white text-4xl text-center top-24 left-0 right-0">
+    Yin Messaging
+</h1>
+<div class="flex flex-col items-center h-full justify-center">
+    {#if clientError}
+        <div class="flex gap-x-2 text-red-500 mb-4">
+            <Icon
+                icon="material-symbols:error-circle-rounded"
+                class="text-2xl"
             />
+            <p>{clientError.message}</p>
         </div>
-        <div>
-            <label
-                for="password"
-                class="block text-gray-700 text-sm font-bold mb-2"
-                >Password</label
+    {/if}
+    {#if isRegistering}
+        <form
+            action="#"
+            on:submit|preventDefault={signUp}
+            class="flex flex-col text-white gap-4 w-full md:w-2/3 lg:w-1/3"
+        >
+            <div class="flex gap-4 border-2 rounded-full px-4 items-center">
+                <Icon icon="mdi:user" class="text-2xl" />
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                    bind:value={username}
+                />
+            </div>
+            <div class="flex w-full gap-4">
+                <div
+                    class="flex gap-4 border-2 rounded-full px-6 items-center w-1/2"
+                >
+                    <input
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name"
+                        class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                        bind:value={firstName}
+                    />
+                </div>
+                <div
+                    class="flex gap-4 border-2 rounded-full px-6 items-center w-1/2"
+                >
+                    <input
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name"
+                        class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                        bind:value={lastName}
+                    />
+                </div>
+            </div>
+            <div class="flex gap-4 border-2 rounded-full px-4 items-center">
+                <Icon icon="mdi:lock" class="text-2xl" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                    bind:value={password}
+                />
+            </div>
+            <div class="flex gap-4 border-2 rounded-full px-4 items-center">
+                <Icon icon="mdi:repeat-variant" class="text-2xl" />
+                <input
+                    type="password"
+                    name="confirm-password"
+                    placeholder="Repeat Password"
+                    class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                    bind:value={passwordConfirm}
+                />
+            </div>
+            <button
+                type="submit"
+                disabled={!username ||
+                    !password ||
+                    !passwordConfirm ||
+                    passwordConfirm !== password}
+                class={!username ||
+                !password ||
+                !passwordConfirm ||
+                passwordConfirm !== password
+                    ? "bg-gray-300 py-2 px-4 rounded font-bold w-full text-lg self-center mt-4"
+                    : "bg-purple-600 py-2 px-4 rounded font-bold w-full text-lg hover:bg-purple-700 self-center mt-4"}
+                >Sign Up</button
             >
-            <input
-                type="password"
-                name="password"
-                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                bind:value={password}
-            />
-        </div>
-        <button on:click={signUp}>Sign Up</button>
-        <button on:click={login}>Login</button>
-    </form>
-{/if}
+        </form>
+    {:else}
+        <form
+            action="#"
+            on:submit|preventDefault={login}
+            class="flex flex-col text-white gap-4 w-full md:w-2/3 lg:w-1/3"
+        >
+            <div class="flex gap-4 border-2 rounded-full px-4 items-center">
+                <Icon icon="mdi:user" class="text-2xl" />
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                    bind:value={username}
+                />
+            </div>
+            <div class="flex gap-4 border-2 rounded-full px-4 items-center">
+                <Icon icon="material-symbols:lock" class="text-2xl" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    class="bg-transparent appearance-none w-full py-2 leading-tight focus:outline-none focus:shadow-outline"
+                    bind:value={password}
+                />
+            </div>
+            <button
+                type="submit"
+                disabled={!username || !password}
+                class={!username || !password
+                    ? "bg-gray-300 py-2 px-4 rounded font-bold w-full text-lg self-center mt-4"
+                    : "bg-purple-600 py-2 px-4 rounded font-bold w-full text-lg hover:bg-purple-700 self-center mt-4"}
+                >Login</button
+            >
+            <p class="text-sm text-center">
+                Don't have an account with us? <span
+                    class="text-purple-400"
+                    on:click={() => {
+                        username = "";
+                        password = "";
+                        isRegistering = true;
+                    }}
+                    on:keydown>Sign up</span
+                >
+            </p>
+        </form>
+    {/if}
+</div>
